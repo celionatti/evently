79b01d45b79e21981816ff90c837f41978194c9c
celionatti <amisuusman@gmail.com>
2025-09-22 20:13:54 +0100

    making changes

--- resources/views/partials/admin-nav.php
+++ resources/views/partials/admin-nav.php
@@ -4,24 +4,22 @@
 
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg navbar-glass">
-    <div class="container-fluid px-4">
-        <div class="d-flex align-items-center gap-3">
-            <button class="btn btn-ghost sidebar-toggle d-lg-none" id="sidebarToggle">
+    <div class="container-fluid">
+        <div class="navbar-left">
+            <button class="btn sidebar-toggle d-lg-none" id="sidebarToggle">
                 <i class="bi bi-list fs-4"></i>
             </button>
-            <a class="navbar-brand d-flex align-items-center gap-1" href="<?= url("/") ?>">
-                <!-- <span class="brand-mark">E</span>
-                <span class="fw-bold text-white">ventlyy.</span> -->
+            <a class="navbar-brand" href="<?= url("/") ?>">
                 <img src="<?= get_image("dist/img/logo.png") ?>" class="img-fluid" width="30px">
-                <span class="fw-bold text-white">ventlyy.</span>
+                <!-- <span class="fw-bold text-white">ventlyy.</span> -->
             </a>
         </div>
 
-        <div class="d-flex align-items-center gap-3">
+        <div class="navbar-right">
             <div class="dropdown">
                 <button class="btn btn-ghost dropdown-toggle" type="button" data-bs-toggle="dropdown">
                     <i class="bi bi-person-circle me-1"></i>
-                    <?= htmlspecialchars(auth()->name . ' ' . auth()->other_name) ?>
+                    <span class="user-name"><?= htmlspecialchars(auth()->name . ' ' . auth()->other_name) ?></span>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li><a class="dropdown-item" href="<?= url("/admin/profile") ?>"><i class="bi bi-person me-2"></i>Profile</a></li>
